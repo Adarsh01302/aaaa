e54239a81f05ca420f21b62a8241705fbe14b1eb
sdac-16 <sdac-16@sdac-16-PC>
2023-05-25 15:16:10 +0530

    222

---
+++ aqaa/src/aqaa/main2.java
@@ -0,0 +1,10 @@
+package aqaa;
+
+public class main2 {
+	public static void main(String[] args) {
+		System.out.println("bsdgfguf");
+		System.out.println("bsdgfguf");
+		System.out.println("bsdgfguf");
+	}
+
+}
